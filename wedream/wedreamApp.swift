53b389fdedeb5415392b85e48d1f045cacf55a24
supreme-gg-gg <145060541+supreme-gg-gg@users.noreply.github.com>
2024-05-06 12:10:07 -0400

    Initial Commit

---
+++ wedream/wedreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  wedreamApp.swift
+//  wedream
+//
+//  Created by Jet Chiang on 2024-05-06.
+//
+
+import SwiftUI
+
+@main
+struct wedreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
